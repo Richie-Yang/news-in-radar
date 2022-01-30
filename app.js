4b72130021013473af3edb9e1f38eda5c0928471
Richie-Yang <richieyang8813@gmail.com>
2022-01-30 20:19:35 +0800

    feat: add Express-Handlebars

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const routes = require('./routes')
+
 const app = express()
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => res.send('<h1>Hello</h1>'))
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(routes)
 
 app.listen(PORT, () => console.info(`Express server is listening at 127.0.0.1:${3000}`))
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => res.send('<h1>Hello</h1>'))
+
+module.exports = router
