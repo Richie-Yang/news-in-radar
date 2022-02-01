31d6e828cffbf571a1b7ce70592ef12176401e88
Richie-Yang <richieyang8813@gmail.com>
2022-02-01 16:59:07 +0800

    feat: add GET/POST /register routes, functions, and view

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,25 @@ module.exports = {
   loginPage: (req, res) => {
     return res.render('login')
   },
+
   login: (req, res, next) => {
     const { email, password } = req.body
     console.log(email, password)
     return res.send('ok')
+  },
+
+  registerPage: (req, res) => {
+    return res.render('register')
+  },
+
+  register: (req, res, next) => {
+    const { 
+      name, email, password, confirmPassword 
+    } = req.body
+
+    console.log(
+      name, email, password, confirmPassword 
+    )
+    return res.redirect('/login')
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const userController = require('../controllers/userController')
 
 router.get('/login', userController.loginPage)
 router.post('/login', userController.login)
+router.get('/register', userController.registerPage)
+router.post('/register', userController.register)
+
 router.get('/', (req, res) => res.redirect('/login'))
 
 module.exports = router
