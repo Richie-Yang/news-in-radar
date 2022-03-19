155971511b64d0560130026cd95866be1af16bc7
Richie-Yang <richieyang8813@gmail.com>
2022-03-19 16:14:05 +0800

    refactor: seperate several checks from registerValidation

--- helpers/validate-helpers.js
+++ helpers/validate-helpers.js
@@ -1,6 +1,33 @@
 const validator = require('validator')
 
 module.exports = {
+  allFieldsCheck: (fields, data, cb) => {
+    // not tested yet
+    const fieldsCheck = fields.every(f => data.includes(f))
+    if (!fieldsCheck) return cb(new Error('信箱, 密碼, 確認密碼欄位都是必填'))
+    return cb(null)
+  },
+
+  emailCheck: (email, cb) => {
+    // not tested yet
+    const emailValidated = validator.isEmail(email)
+    if (!emailValidated) return cb(new Error('信箱格式不符'))
+    return cb(null)
+  },
+
+  passwordCheck: (password, confirmPassword, cb) => {
+    // not tested yet
+    const errors = []
+    const passwordValidated = validator.isLength(password, { min: 6 })
+    const twinValidated = validator.equals(password, confirmPassword)
+
+    if (!passwordValidated) errors.push('密碼長度不足')
+    if (!twinValidated) errors.push('密碼欄位不符')
+
+    if (errors.length) return cb(new Error(errors))
+    return cb(null)
+  },
+
   registerValidation: data => {
     const errors = []
     const { email, password, confirmPassword } = data
