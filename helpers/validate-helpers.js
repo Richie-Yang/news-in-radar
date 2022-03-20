ea8b09a356fa2ad9ef202f8010b12cb4c1d107f0
Richie-Yang <richieyang8813@gmail.com>
2022-03-20 21:44:33 +0800

    feat: polish validate-helpers

--- helpers/validate-helpers.js
+++ helpers/validate-helpers.js
@@ -1,48 +1,26 @@
 const validator = require('validator')
 
 module.exports = {
-  allFieldsCheck: (fields, data, cb) => {
-    // not tested yet
-    const fieldsCheck = fields.every(f => data.includes(f))
-    if (!fieldsCheck) return cb(new Error('信箱, 密碼, 確認密碼欄位都是必填'))
-    return cb(null)
-  },
-
-  emailCheck: (email, cb) => {
-    // not tested yet
-    const emailValidated = validator.isEmail(email)
-    if (!emailValidated) return cb(new Error('信箱格式不符'))
-    return cb(null)
-  },
-
-  passwordCheck: (password, confirmPassword, cb) => {
-    // not tested yet
-    const errors = []
-    const passwordValidated = validator.isLength(password, { min: 6 })
-    const twinValidated = validator.equals(password, confirmPassword)
-
-    if (!passwordValidated) errors.push('密碼長度不足')
-    if (!twinValidated) errors.push('密碼欄位不符')
-
-    if (errors.length) return cb(new Error(errors))
-    return cb(null)
-  },
-
-  registerValidation: data => {
+  registerValidation: function (data) {
     const errors = []
     const { email, password, confirmPassword } = data
-
     if (!email || !password || !confirmPassword) {
-      return '信箱, 密碼, 確認密碼欄位都是必填'
+      throw new Error('信箱, 密碼, 確認密碼欄位都是必填')
     }
 
     const emailValidated = validator.isEmail(email)
     const passwordValidated = validator.isLength(password, { min: 6 })
     const twinValidated = validator.equals(password, confirmPassword)
+    const lowercaseValidated = validator.matches(password, /[a-z]/)
+    const uppercaseValidated = validator.matches(password, /[A-Z]/)
+    const numberValidated = validator.matches(password, /[1-9]/)
 
     if (!emailValidated) errors.push('信箱格式不符')
     if (!passwordValidated) errors.push('密碼長度不足')
-    if (!twinValidated) errors.push('密碼欄位不符')
+    if (!twinValidated) errors.push('兩個密碼欄位並不相符')
+    if (!lowercaseValidated) errors.push('密碼需含小寫英文')
+    if (!uppercaseValidated) errors.push('密碼需含大寫英文')
+    if (!numberValidated) errors.push('密碼需含數字')
 
     return errors.length ? errors.join() : 'ok'
   }
